88fb3652b8592778f51a76e756eb41d55e51136d
gon0821 <121909372+gon0821@users.noreply.github.com>
2023-12-11 14:08:10 +0000

    Finish ch05

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,14 @@ class StaticPagesController < ApplicationController
   end
 
   def help
+    # => app/views/static_pages/help.html.erb
   end
-
+  
   def about
+    # => app/views/static_pages/about.html.erb
+  end
+  
+  def contact
+    # => app/views/static_pages/contact.html.erb
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,20 +2,29 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    # GET /static_pages/help
+    get help_path
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
-
+  
   test "should get about" do
-    get static_pages_about_url
+    # GET /static_pages/about
+    get about_path
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get contact" do
+    # GET /static_pages/contact
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
